a2e3ca3de9503439c7d02c7f05c3b4a54a4b14f7
corban <corban30303@hotmail.com>
2020-09-01 00:00:06 +1200

    ignore static

--- main/settings.py
+++ main/settings.py
@@ -74,10 +74,6 @@ TEMPLATES = [
     },
 ]
 
-TEMPLATE_DIRS = (
-    os.path.join("dist"),
-)
-
 WSGI_APPLICATION = 'main.wsgi.application'
 
 
@@ -131,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
